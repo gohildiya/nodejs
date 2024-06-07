6275b2093a3f5652967632c82e29f730e2dffb8d
gohildiya <gohildiyas@email.com>
2024-06-07 21:55:24 +0530

    renamed

---
+++ 17_delete_file.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server = http.createServer(function(request,response)
+{
+    var requested_url = url.parse(request.url,true);
+    var filename = requested_url.pathname.substring(1); // /friends.txt
+    console.log(filename);
+    fs.unlink(filename,function(error){
+        if(error){
+            response.write('file cannot be deleted');
+            response.end();
+        }
+        else{
+            response.write('file deleted successfully');
+            response.end();
+        }
+    });
+});
+server.listen(5000);
+console.log('ready to accept request...');

31b1992c13bc96d80c18637cbb969f7af8a199ae
gohildiya <gohildiyas@email.com>
2024-06-18 19:54:02 +0530

    contactus

--- pug/lesson1.js
+++ pug/lesson1.js
@@ -12,5 +12,25 @@ app.get('/two',function(req,res){
     res.render('two');
 });
 
+app.get('/contactus',function(req,res){
+    let contact = {companyname : 'Explore Infotech',
+        branch : 'Mern Stack',
+        city : 'Ahemdabad',
+        pincode : '380001',
+        contactno : '8866576821',
+        email : 'gohildiya@gmail.com',
+        parking : '2 Wheeler parking available'
+    }
+    res.render('contactus',contact);
+});
+
+app.get('/loop1',function(req,res){
+    let state = {
+        district: ["Ahmedabad","Amreli","Anand","Aravalli","Banaskantha","Bharuch","Bhavnagar","Botad","Chhota Udaipur","Dahod","Dang","Devbhoomi Dwarka","Gandhinagar","Gir Somnath","Jamnagar","Junagadh","Kheda","Kutch","Mahisagar","Mehsana","Morbi","Narmada","Navsari","Panchmahal","Patan","Porbandar","Rajkot","Sabarkantha","Surat","Surendranagar","Tapi","Vadodara","Valsad"
+        ]
+    };
+    res.render('loop1', state);
+});
+
 app.listen(5000);
 console.log('ready to accept request..');

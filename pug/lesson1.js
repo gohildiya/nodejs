f5b5bbe6777779f4e33cab363f12dd87c3c509c9
gohildiya <gohildiyas@email.com>
2024-06-19 21:33:49 +0530

    loop

--- pug/lesson1.js
+++ pug/lesson1.js
@@ -32,5 +32,26 @@ app.get('/loop1',function(req,res){
     res.render('loop1', state);
 });
 
+app.get('/student',function(req,res){
+    let data = {
+        students :  {
+            name: " Gohil Diya", html: 85, css: 87, javascript: 90, get total() {
+                return this.html + this.css + this.javascript;
+            }
+    }
+}
+    res.render('student',data);
+});
+
+app.get('/loop2',function(req,res){
+    let list = {
+        student : ["Aarav Sharma", "Vivaan Singh", "Aditya Agarwal", "Vihaan Iyer", "Arjun Menon",
+            "Sai Prasad", "Reyansh Gupta", "Krishna Rao", "Ishaan Patel", "Shaurya Reddy",
+            "Dhruv Desai", "Ritvik Nair", "Ananya Mehta", "Aditi Chandra", "Ishita Bhatnagar",
+            "Diya Kapoor", "Saanvi Joshi", "Pari Srivastava", "Myra Bansal", "Anaya Roy"]
+    }
+    res.render('loop2',list);
+});
+
 app.listen(5000);
 console.log('ready to accept request..');
